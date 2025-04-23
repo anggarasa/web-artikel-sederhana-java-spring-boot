58fa0d6563921de1d3735f9cec6d47019c173f0f
anggarasa <anggasaputra6259@gmail.com>
2025-04-23 09:31:29 +0700

    membuat web artikel dengan java spring boot

---
+++ src/main/java/com/anggara/webartikel/WebArtikelApplication.java
@@ -0,0 +1,13 @@
+package com.anggara.webartikel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebArtikelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebArtikelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anggara/webartikel/WebArtikelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anggara.webartikel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebArtikelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
