2c2cd17a4ff456daaf33c04d8c6593dfefb099e1
anggarasa <anggasaputra6259@gmail.com>
2025-04-23 09:33:46 +0700

    install tailwindcss

--- src/main/java/com/anggara/webartikel/WebArtikelApplication.java
+++ src/main/java/com/anggara/webartikel/WebArtikelApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebArtikelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebArtikelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebArtikelApplication.class, args);
+    }
 
 }
